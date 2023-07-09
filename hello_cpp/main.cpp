cd1f08336e88ad361229b219ada1e17c2fd74f8e
Dovbar <baror2009@gmail.com>
2023-07-09 10:58:29 +0300

    "update"

--- hello_cpp/main.cpp
+++ hello_cpp/main.cpp
@@ -1,14 +1,39 @@
 
 #include "Header.h"
+#include <cstdlib>
 #include <iostream>
+#include <stdio.h>
+
+struct Cat{
+	char * name;
+	int age;
+	Cat(const char* name)
+	{
+		this->name = (char*)malloc(sizeof(char) * (strlen(name) + 1));
+		strcpy(this->name, name);
+		age = 0;
+	}
+	~Cat()
+	{
+		std::cout << "cat destroy" << std::endl;
+		free(name);
+
+	}
+};
+
+
 
  int main() { 
-	 print_num(5); // -> Which version will be called ? 
- print_num(5.f); // -> Which version will be called ? 
- print_num(5.); // -> Which version will be called ? 
+	// print_num(5); // -> Which version will be called ? 
+ //print_num(5.f); // -> Which version will be called ? 
+ //print_num(5.); // -> Which version will be called ? 
 
 
- //std::cout << inverse_num(0.1);
- std::cout << inverse_num(5.1);
+ ////std::cout << inverse_num(0.1);
+ //std::cout << inverse_num(5.1);
+
+	 Cat c("nora");
+	 
+
  return 0;
  }
