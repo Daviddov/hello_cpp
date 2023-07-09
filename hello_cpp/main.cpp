efd283e492c1cb9c78e1b316b0827933fd460a13
Dovbar <baror2009@gmail.com>
2023-07-09 11:11:50 +0300

    Cat

--- hello_cpp/main.cpp
+++ hello_cpp/main.cpp
@@ -33,6 +33,13 @@ struct Cat{
  //std::cout << inverse_num(5.1);
 
 	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
+	 Cat c("nora");
 	 
 
  //moshe changes..
